d916a2a88e7f248ed9ac299a6d1a6bc203177083
Sanzid <sanzid94@gmail.com>
2024-01-20 09:28:39 +0600

    Project initial Setup Done

---
+++ Chaati.Maui/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Chaati.Maui
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
